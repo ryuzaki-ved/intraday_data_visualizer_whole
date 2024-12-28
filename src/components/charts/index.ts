2a8056ddfdb62e54b22bf831fdff57f70011cb86
ryuzaki-ved <vedantdapt@gmail.com>
2024-12-28 18:16:15 +0530

    Code cleaned

--- src/components/charts/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all chart components
-
-export { default as CandlestickChart } from './CandlestickChart'
-export { default as LineChart } from './LineChart'
-export { default as VolumeChart } from './VolumeChart'
-export { default as ChartContainer } from './ChartContainer' 
--- src/components/ui/index.ts
+++
@@ -1,7 +0,0 @@
-// Export all UI components
-
-export { default as Button } from './Button'
-export { default as Input } from './Input'
-export { default as Select } from './Select'
-export { default as Card, CardHeader, CardTitle, CardSubtitle, CardContent, CardFooter } from './Card'
-export { default as LoadingSpinner, LoadingOverlay, LoadingSkeleton } from './LoadingSpinner' 
